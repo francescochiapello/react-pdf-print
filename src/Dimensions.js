6f23437b71f7e12ddcf5458a78636145f7523079
Frame System <develop@framesystem.it>
2019-05-03 13:28:56 +0200

    First commit

---
+++ src/Dimensions.js
@@ -0,0 +1,57 @@
+export default = [
+  {
+    name: 'A0',
+    width: '841',
+    height: '1189'
+  },
+  {
+    name: 'A1',
+    width: '594',
+    height: '841'
+  },
+  {
+    name: 'A2',
+    width: '420',
+    height: '594'
+  },
+  {
+    name: 'A3',
+    width: '297',
+    height: '420'
+  },
+  {
+    name: 'A4',
+    width: '210',
+    height: '297'
+  },
+  {
+    name: 'A5',
+    width: '148',
+    height: '210'
+  },
+  {
+    name: 'A6',
+    width: '105',
+    height: '148'
+  },
+  {
+    name: 'A7',
+    width: '74',
+    height: '105'
+  },
+  {
+    name: 'A8',
+    width: '52',
+    height: '74'
+  },
+  {
+    name: 'A9',
+    width: '37',
+    height: '52'
+  },
+  {
+    name: 'A10',
+    width: '26',
+    height: '37'
+  }
+]
