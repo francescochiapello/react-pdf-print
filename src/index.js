195092a5586787235b659a14b8883adb40b6e36c
Frame System <develop@framesystem.it>
2019-05-03 16:01:12 +0200

    General correction to production build version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import html2canvas from 'html2canvas'
+import JSPDF from 'jspdf'
+import toPx from 'unit-to-px'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-serviceWorker.unregister()
+export const print = async (pages) => {
+  let images = []
+
+  await Promise.all(pages.map(async value => {
+    const input = document.getElementById(value)
+
+    const promise = new Promise(
+      resolve => html2canvas(input).then(canvas => {
+        resolve(canvas)
+      })
+    )
+
+    await Promise.all([promise]).then(canvas => {
+      const image = canvas[0].toDataURL('image/png')
+      images.push(image)
+    })
+  }))
+
+  images.length > 0 && download(images)
+}
+
+const download = images => {
+  const pdf = new JSPDF()
+  const pages = document.getElementById('pages').offsetHeight / toPx('297mm')
+
+  for (let i = 0; i < Math.trunc(pages); i++) {
+    i > 0 && i < Math.trunc(pages) && pdf.addPage()
+    pdf.addImage(images[i], 'PNG', 0, 0)
+  }
+  pdf.save('download.pdf')
+}
+
+export default ({ children }) => {
+  return (
+    <div id='pages'>
+      {children}
+    </div>
+  )
+}
